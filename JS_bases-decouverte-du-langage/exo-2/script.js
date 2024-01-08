796216aec27d6b72da875bcc545adafbc0344083
SJSzyn <155622276+SJSzyn@users.noreply.github.com>
2024-01-08 16:38:12 +0100

    Add files via upload

---
+++ JS_bases-decouverte-du-langage/exo-2/script.js
@@ -0,0 +1,67 @@
+// you can write js here
+console.log('exo-2');
+
+// 1 
+
+const myDate = new Date(); // Variable that is assigned the object Date to find the local time
+
+let weekdayMsg = `It's the weekday! Go work!`;  // variable for the Weekdays
+let weekendMsg = `It's the weekend! Don't think of work!`; // Variable for the weekends
+
+console.log(myDate); // Console will show the local time
+
+// 2 
+
+let dayOfWeek = myDate.getDay(); // declaring a variable with is used to get the day of the week as an integer value ranging from 0 to 6, where 0 represents Sunday
+console.log(dayOfWeek); // If it's Monday it'll show 1, if Tuesday 2...
+
+if (dayOfWeek >= 1 && dayOfWeek <= 5) { 
+    console.log(weekdayMsg);
+} else {
+    console.log(weekendMsg);
+}
+
+/* Conditions of when the messages will show, dayOfWeek >= 1 & <= 5, 
+this means it goes from 1 to 5 for the first condition. 
+If it is not achieved the other statement will be shown
+*/
+
+// 3
+
+let endOfWeekdayHour = myDate.getHours(); // declaring a variable to find the hours in the variable myDate
+console.log(endOfWeekdayHour); // Will show me the local hour
+
+if ((dayOfWeek >= 1 && dayOfWeek <= 5) || (dayOfWeek === 5 && endOfWeekdayHour < 17)) {  
+    console.log(weekdayMsg);
+} else {
+    console.log(weekendMsg);
+}
+
+/* Same as the previous one except We have added an extra conidition in the first part.
+We used || 'or' to make it so both conditions must be met.
+*/
+
+// 4
+
+if ((dayOfWeek >= 1 && dayOfWeek <= 5) || (dayOfWeek === 5 && endOfWeekdayHour < 17) || (dayOfWeek === 1 && endOfWeekdayHour > 8)) { // Added a 3rd condition
+    console.log(weekdayMsg);
+} else {
+    console.log(weekendMsg);
+}
+
+// testing
+
+let isTesting = true; 
+
+if (isTesting) {
+  let testDay = prompt("day");
+  let testHour = prompt("hr");
+  dayOfWeek = parseInt(testDay);
+  endOfWeekdayHour = parseInt(testHour);
+}
+
+if ((dayOfWeek >= 1 && dayOfWeek <= 5) || (dayOfWeek === 5 && endOfWeekdayHour < 17) || (dayOfWeek === 1 && endOfWeekdayHour > 8)) {
+  console.log(weekdayMsg);
+} else {
+  console.log(weekendMsg);
+}
